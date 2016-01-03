da909c578d6666772a6e1ab3460868084761ea14
Paul Kubala <paulisdaman@gmail.com>
2016-01-02 17:26:03 -0700

    adding some base line code

--- ballBounce.js
+++ ballBounce.js
@@ -1,2 +1,13 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+function drawBall() {
+
+}
+
+
+function draw() {
+  drawBall();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(draw);
+}
